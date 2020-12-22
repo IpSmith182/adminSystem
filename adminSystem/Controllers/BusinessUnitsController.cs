d076d707e0e4763367fd8ab69f1a5d41991e6f64
Iain Smith <Smith.ip182@gmail.com>
2020-12-22 11:31:39 +0000

    first commit

---
+++ adminSystem/Controllers/BusinessUnitsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using adminSystem.Models;
+
+namespace adminSystem.Controllers
+{
+    public class BusinessUnitsController : Controller
+    {
+        private adminDBEntities db = new adminDBEntities();
+
+        // GET: BusinessUnits
+        public ActionResult Index()
+        {
+            //return View(db.BusinessUnits.ToList());
+
+            var bu = db.BusinessUnits.Where(b => b.Active == true);
+            return View(bu);
+        }
+
+        // GET: BusinessUnits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BusinessUnit businessUnit = db.BusinessUnits.Find(id);
+            if (businessUnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(businessUnit);
+        }
+
+        public ActionResult Staff(int id = 0)
+        {
+            var sbu = db.Staffs.Where(s => s.businessUnitId == id);
+            if (sbu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sbu);
+        }
+
+        // GET: BusinessUnits/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BusinessUnits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "businessUnitId,businessUnitCode,title,description,officeAddress1,officeAddresss2,officeAddress3,officePostCode,officeContact,officePhone,officeEmail,Active")] BusinessUnit businessUnit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BusinessUnits.Add(businessUnit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(businessUnit);
+        }
+
+        // GET: BusinessUnits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BusinessUnit businessUnit = db.BusinessUnits.Find(id);
+            if (businessUnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(businessUnit);
+        }
+
+        // POST: BusinessUnits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "businessUnitId,businessUnitCode,title,description,officeAddress1,officeAddresss2,officeAddress3,officePostCode,officeContact,officePhone,officeEmail,Active")] BusinessUnit businessUnit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(businessUnit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(businessUnit);
+        }
+
+        // GET: BusinessUnits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BusinessUnit businessUnit = db.BusinessUnits.Find(id);
+            if (businessUnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(businessUnit);
+        }
+
+        // POST: BusinessUnits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BusinessUnit businessUnit = db.BusinessUnits.Find(id);
+            businessUnit.Active = false;
+            //db.BusinessUnits.Remove(businessUnit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    }
+}
---
+++ adminSystem/Controllers/StaffsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using adminSystem.Models;
+
+namespace adminSystem.Controllers
+{
+    public class StaffsController : Controller
+    {
+        private adminDBEntities db = new adminDBEntities();
+
+        // GET: Staffs
+        public ActionResult Index()
+        {
+            //var staffs = db.Staffs.Include(s => s.BusinessUnit);
+            //return View(staffs.ToList());
+
+            var stf = db.Staffs.Where(b => b.Active == true);
+            return View(stf);
+        }
+
+        // GET: Staffs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // GET: Staffs/Create
+        public ActionResult Create()
+        {
+            ViewBag.businessUnitId = new SelectList(db.BusinessUnits, "businessUnitId", "businessUnitCode");
+            return View();
+        }
+
+        // POST: Staffs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "staffId,businessUnitId,staffCode,firstName,middleName,lastName,dob,startDate,profile,emailAddress,PhotoUrl,Active")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Staffs.Add(staff);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.businessUnitId = new SelectList(db.BusinessUnits, "businessUnitId", "businessUnitCode", staff.businessUnitId);
+            return View(staff);
+        }
+
+        // GET: Staffs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.businessUnitId = new SelectList(db.BusinessUnits, "businessUnitId", "businessUnitCode", staff.businessUnitId);
+            return View(staff);
+        }
+
+        // POST: Staffs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "staffId,businessUnitId,staffCode,firstName,middleName,lastName,dob,startDate,profile,emailAddress,PhotoUrl,Active")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(staff).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.businessUnitId = new SelectList(db.BusinessUnits, "businessUnitId", "businessUnitCode", staff.businessUnitId);
+            return View(staff);
+        }
+
+        // GET: Staffs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Staff staff = db.Staffs.Find(id);
+            if (staff == null)
+            {
+                return HttpNotFound();
+            }
+            return View(staff);
+        }
+
+        // POST: Staffs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Staff staff = db.Staffs.Find(id);
+            staff.Active = false;
+            //db.Staffs.Remove(staff);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
